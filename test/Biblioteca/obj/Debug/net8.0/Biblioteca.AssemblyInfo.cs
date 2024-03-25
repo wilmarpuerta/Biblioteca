f012be16704b9c016fe335a3ada95258dcfebce5
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-03-25 01:05:40 -0500

    Mensaje de eliminacion con estilos

--- test/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ test/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d75fc75fc11b5f7e868e3c804705c8919af017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1868b55b854b2c6324cfb9b22ddf2986f658a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
