164976300720dd59ea2de68844a04cc77026c449
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-03-24 23:07:35 -0500

    Vista de usuario con estilos

--- test/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ test/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cd2f427999a143fee81bb3e2aebfafe253ba40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40529c710d911de09f10417fa663e42c279c5f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
